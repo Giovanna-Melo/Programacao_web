efd71dcab33c74cf01d5904ee92e1a693adc8f3f
Giovanna-Melo <gikailany@gmail.com>
2024-12-13 20:50:10 -0300

    Receita 1

---
+++ next/pages/ex_1_e_2_receita_1.js
@@ -0,0 +1,19 @@
+export function Ex_one (){
+    return (
+        <div> 
+            <h2>
+            Exercício 1: Primeira função, não default
+            </h2>
+        </div>
+    )
+};
+
+export function Ex_two (){
+    return (
+        <div> 
+            <h2>
+            Exercício 2: Segunda função, não default
+            </h2>
+        </div>
+    )
+}
---
+++ next/pages/index.js
@@ -0,0 +1,12 @@
+import {Ex_two} from './ex_1_e_2_receita_1';
+
+export default function Home(){
+    return (
+        <div> 
+            <h2>
+                Viva Santana!
+            </h2>
+            <Ex_two/>
+        </div>
+    )
+}
