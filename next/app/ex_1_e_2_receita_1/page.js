b288d2e73552bb78f1e56a612670914fbb0bd36a
Giovanna-Melo <gikailany@gmail.com>
2025-01-04 21:44:45 -0300

    Atualizações da receita 1

---
+++ next/app/ex_1_e_2_receita_1/page.js
@@ -0,0 +1,20 @@
+import {Ex_two} from "../page";
+export default function Ex_one_and_two (){
+    return (
+        <div> 
+            <h1>
+                Exercícios 1 e 2    
+            </h1>
+            <h2>
+                Testando funções defaults e não defaults
+            </h2>
+            <h3>
+                ** Eis outra função default que contém uma função não default em seu corpo. **
+            </h3>
+            <h3>
+                Exercício 2:
+            </h3>
+            <Ex_two/>
+        </div>
+    )
+};
---
+++ next/app/ex_1_receita_1/page.js
@@ -0,0 +1,9 @@
+export default function Ex_one (){
+    return (
+        <div> 
+            <h2>
+                Eis uma função default
+            </h2>
+        </div>
+    )
+};
