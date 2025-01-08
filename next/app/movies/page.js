d285fc892822644c0ad369c1e87e1d1d9250b3bf
Giovanna-Melo <gikailany@gmail.com>
2025-01-08 13:05:39 -0300

    Receita 3

---
+++ next/app/movies/page.js
@@ -0,0 +1,26 @@
+/* Abordagem alternativa
+function transformar(elemento){
+    return (<div>{elemento.Title} --- {elemento.Year}</div>) 
+}*/
+
+export default async function Home({searchParams}){
+    const {titleSearchKey = 'bagdad', type = 'movie'} = await searchParams
+    const res = await fetch(`http://www.omdbapi.com/?apikey=6b8a8a20&s=${titleSearchKey}&type=${type}`)
+    const data = await res.json()
+
+    return (
+        <div>
+            <div>
+                {data.Search.map( (m) => 
+                    <div key={m.imdbID}>
+                        {m.Title} --- {m.Year}
+                        <div>
+                            <img src={m.Poster !== "N/A" ? m.Poster: "/placeholder.jpg"} alt={m.Title} width="150" />
+                        </div>
+                    </div>
+                )}                
+            </div>
+        </div>
+    )
+}
+
