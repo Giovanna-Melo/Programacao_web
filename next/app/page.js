e1c2782176205c57c75733981227df093d73a8f1
Giovanna-Melo <gikailany@gmail.com>
2025-01-07 22:49:08 -0300

    Receita 2a

--- next/app/page.js
+++ next/app/page.js
@@ -1,15 +1,22 @@
-export default function Home(){
+"use client";
+
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+
+export default function Home() {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []); //Receita 2a
+
     return (
         <div>
-            <div>Menu principal</div>
-            <div>
-                <h1>
-                    Viva Santana!
-                </h1>
-
-            </div>
-        </div>       
-    )
+            <h1>Viva Santana!</h1>
+            <Link href="/novarota">Rota 1</Link> <br /><br />
+            {isClient && <a href="/novarota">Rota 1, jeito antigo</a>}
+        </div>
+    );
 }
 
 export function Ex_two (){
